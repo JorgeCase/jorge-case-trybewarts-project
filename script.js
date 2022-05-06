21029ab812def1b974c07c8c7acee126eaaae506
leandro-miranda <leandromiranda2010@gmail.com>
2022-05-05 21:07:50 -0300

    alterações

--- script.js
+++ script.js
@@ -1,33 +1,46 @@
-const agreeLabel = document.getElementById("label-infos");
-const agree = document.getElementById("agreement");
-const submit = document.getElementById("submit-btn");
-let counterDisplay = document.getElementById("counter");
-let textarea = document.getElementsByClassName("textareaBox")[0];
+const agreeLabel = document.getElementById('label-infos');
+const agree = document.getElementById('agreement');
+const submit = document.getElementById('submit-btn');
+const counterDisplay = document.getElementById('counter');
+const textarea = document.getElementsByClassName('textareaBox')[0];
 
 submit.disabled = true;
 
 function disabled() {
-
-  console.log(submit.disabled)
+  console.log(submit.disabled);
 
   if (submit.disabled === true) {
     submit.disabled = false;
-  }
-
-  else if (submit.disabled === false){
+  } else if (submit.disabled === false) {
     submit.disabled = true;
   }
 }
 
 function counter() {
-  let textareaContent = textarea.value;
-  let textareaContentLength = textareaContent.length;
+  const textareaContent = textarea.value;
+  const textareaContentLength = textareaContent.length;
 
   counterDisplay.innerHTML = 500 - textareaContentLength;
-
 }
 
-agree.addEventListener("click", disabled);
-agreeLabel.addEventListener("click", disabled);
-textarea.addEventListener("input", counter)
+agree.addEventListener('click', disabled);
+agreeLabel.addEventListener('click', disabled);
+textarea.addEventListener('input', counter);
 
+function valueInputs() {
+  const emailInput = document.getElementById('email');
+  const passwordInput = document.getElementById('password');
+  const clickButton = document.getElementById('enter');
+
+  clickButton.addEventListener('click', () => {
+    if (
+      emailInput.value === 'tryber@teste.com'
+      && passwordInput.value === '123456'
+    ) {
+      alert('Olá, Tryber!');
+    } else {
+      alert('Email ou senha inválidos.');
+    }
+  });
+}
+valueInputs();
